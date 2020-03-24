4b87a7ca527b6f063dff594f0f9f8c598749e58b
unknown <ian0198@gmail.com>
2020-03-24 18:04:01 -0300

    Feat: instagram and videos response

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const { google } = require('googleapis');
 
-let app = require('./config/custom-express')();
+const app = require('./config/custom-express')();
+const puppeteer = require('puppeteer');
+const resolve = require('path').resolve
+let VIDEOS_TIME_STAMP
+let INSTAGRAM_TIME_STAMP
 
 app.listen(9000, async () => {
     console.log('Servidor rodando na porta 9000.');
 })
 
-app.get('/', async function (request, response) {
+app.get('/videos', async function (request, response) {
+    const videos = await getLastYoutubeVideos()
+    response.status(200).send(videos)
+})
+
+app.get('/instagram', async function (request, response) {
+    const lastPost = await getLastInstagramPost()
+    response.status(200).sendFile(lastPost)
+})
+
+async function getLastYoutubeVideos() {
     const globalPlaylist = 'UUx2tsANnhacjiqisNmPnl-Q'
     let playlistItemsResponse = await google.youtube('v3').playlistItems.list({
         part: 'snippet',
@@ -24,5 +38,17 @@ app.get('/', async function (request, response) {
         return video;
     });
 
-    response.status(200).send(playlistItemsResponse);
-})
+    return playlistItemsResponse
+}
+
+async function getLastInstagramPost() {
+    const imageName = 'lastpost.png'
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.instagram.com/spoktc/');
+    await page.click('#react-root > section > main > div > div._2z6nI > article > div:nth-child(1) > div > div:nth-child(1) > div:nth-child(1)')
+    await page.waitFor(2000)
+    await page.screenshot({ 'path': imageName, 'clip': { 'x': 40, 'y': 520, 'width': 380, 'height': 380 } });
+    await page.close()
+    return resolve(imageName)
+}
